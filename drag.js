a1e2dd7e77e4398cb14de2c715721a780f929272
1541609448 <1541609448@qq.com>
2018-02-26 21:19:01 +0800

    drag.js5

--- drag.js
+++ drag.js
@@ -2,10 +2,21 @@
 function drag(id){
   var obj=document.getElementById('id');
   obj.onmousedown=function(e){
+    var that=this;
     var ev=e||window.event;
     var disX=ev.clientX-this.offsetLeft;
     var disY=ev.clientY-this.offsetTop;
+      document.onmousemove=function(e){
+        var ev=e||window.event;
+        this.style.left=e.clientX-disX+"px";
+        this.style.top=e.clientY-disY+"px";
 
+      }
+      document.onmouseup=function(){
+        document.onmouseup=null;
+        document.onmousemove=null;
+        
+      }
   }
 
 }
