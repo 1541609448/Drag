5087fb07ffa77978756d48e6d44784198799267d
1541609448 <1541609448@qq.com>
2018-02-26 20:44:11 +0800

    drag4 index.html

--- drag.js
+++ drag.js
@@ -1,7 +1,10 @@
 //拖拽
 function drag(id){
   var obj=document.getElementById('id');
-  obj.onmousedown=function(){
+  obj.onmousedown=function(e){
+    var ev=e||window.event;
+    var disX=ev.clientX-this.offsetLeft;
+    var disY=ev.clientY-this.offsetTop;
 
   }
 
